38e038625d9a958dfba1342f4c4a3c55b76b1b9e
Your Name <you@example.com>
2018-12-17 11:09:33 +0800

    config and views

---
+++ config/database.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var dbconfig = {     
+  connectionLimit : 10,
+  host     : '172.16.101.27',       
+  user     : 'pk4yo',              
+  password : 'zzeP6GAkZ5qt',       
+  port     : '3306',                   
+  database : 'hdf_dev' 
+}; 
+ 
+module.exports = dbconfig;
