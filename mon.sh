d8d219543766873586aabd9fd48e5f14562eb8e0
Your Name <zhanghui@pk4yo.com>
2019-04-24 12:42:40 +0800

    deconfig

---
+++ mon.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+pn=`ps aux | grep "node ./bin/www" | grep -v "grep" | awk 'NR==1{print $2}' | wc -l`
+if [ $pn -eq 0 ];then
+	cd /var/www/haidafu
+	nohup npm start > nohup.out 2>&1 &
+	echo "------ restart at `date` ------" >> nohup.out
+fi
+
